9501e9d208f790e390705215e5765996b3e3a613
Bradley Bossard <bradleybossard@gmail.com>
2016-06-11 20:49:49 +0000

    Example to take an input number and double it

--- chapter4/main.go
+++ chapter4/main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		a = 5
-		b = 10
-		c = 15
-	)
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
 }
