930b3800745b8b92c2341fed23c0cb7a22346ccd
Bradley Bossard <bradleybossard@gmail.com>
2016-06-11 20:48:53 +0000

    Show alternative multiple variable declaration

--- chapter4/main.go
+++ chapter4/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	const x string = "Hello World"
-	fmt.Println(x)
+	var (
+		a = 5
+		b = 10
+		c = 15
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
